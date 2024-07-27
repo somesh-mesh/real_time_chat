3dca03ca6c1f5766913bda2fb2e68602c5c90904
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-07-28 00:20:09 +0530

    added code

---
+++ real_time_chat/src/components/login.jsx
@@ -0,0 +1,22 @@
+const Login = () => {
+    return (
+        <div>
+            <div>
+                <h1>Login</h1>
+                <div>
+                    <input type="Mobile Number"/>
+                    <label htmlFor="">Mobile Number</label>
+                </div>
+                <div>
+                    <input type="Password"/>
+                    <label htmlFor="">Your Password</label>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
